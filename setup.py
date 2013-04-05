c6685bda3d70687cab31044e19bdb31b276fb0e4
Saul <scommab@gmail.com>
2013-04-05 02:37:59 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from pyxbmc import __version__
+
+setup(
+    name="pyxbmc",
+    version=__version__,
+    description="",
+    license="Apache2",
+    url="none",
+    maintainer="saul",
+    maintainer_email="scommab@gmail.com",
+    packages=["pyxbmc"]
+)
